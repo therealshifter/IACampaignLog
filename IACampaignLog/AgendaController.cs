49a99785a0ae34394f849b229904de9c591a4bea
therealshifter <therealshifter@gmail.com>
2016-08-29 18:22:06 +1000

    Add files via upload
    Added AgendaController file

---
+++ IACampaignLog/AgendaController.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IACampaignLog
+{
+   public class AgendaController : CardSetController<Agenda>
+   {
+      public AgendaController() : base(ConfigController.AgendaResourcePath(),
+                                       (x) => Agenda.Deserialise(x))
+      {
+         
+      }
+      
+      public static AgendaController GetInstance()
+      {
+         if (_instance == null)
+         {_instance = new AgendaController();}
+         return (AgendaController)_instance;
+      }
+      
+      public Agenda FindAgendaWithId(int id)
+      {
+         return base.FindTFromAllSetsWithId(id);
+      }
+      
+      public IList<Agenda> FindAgendaInSetWithName(int setId, string name)
+      {
+         return base.FindTInSetWithName(setId, name);
+      }
+      
+      public Agenda AddAgenda(CardSet<Agenda> agendaSet, string name, int influence, Agenda.AgendaType agendaType)
+      {
+         if (string.IsNullOrEmpty(name))
+         {throw new ArgumentException("Agenda name cannot be null or empty");}
+         
+         return base.AddToSet(agendaSet, (x) => new Agenda(x, name, influence, agendaType));
+      }
+      
+   }
+}
+
